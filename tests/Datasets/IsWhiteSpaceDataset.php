a1bce461dd3beeb75c082b9ccbf7e159d44bb959
Yunus Emre Deligöz <ye@deligoz.me>
2023-03-03 09:44:39 +0300

    feat: Add tests for the Guard::isWhiteSpace() method
    This commit adds new test cases for the Guard::isWhiteSpace() method to the IsWhiteSpaceTest.php file. The new test cases cover passing and failing scenarios with different input values, including whitespace characters, non-whitespace characters, and non-string input types. The IsWhiteSpaceDataset.php file has also been updated to include the new test cases.

--- tests/Datasets/IsWhiteSpaceDataset.php
+++ tests/Datasets/IsWhiteSpaceDataset.php
@@ -3,9 +3,17 @@
 declare(strict_types=1);
 
 dataset(passingCasesDataset(filePath: __FILE__), [
-    [],
+    "(' ')"      => [' '],
+    '("\n\t\r")' => ["\n\t\r"],
 ]);
 
 dataset(failingCasesDataset(filePath: __FILE__), [
-    [],
+    "('Abc123')"        => ['Abc123', 'Expected whitespace characters only. Got: "Abc123" (string)'],
+    "('Hello, world!')" => ['Hello, world!', 'Expected whitespace characters only. Got: "Hello, world!" (string)'],
+    '((""))'            => ['', 'Expected whitespace characters only. Got: "" (string)'],
+    '(null)'            => [null, 'Expected a string. Got: null (null)'],
+    '([1, 2, 3])'       => [[1, 2, 3], 'Expected a string. Got: array (array)'],
+    '(new stdClass())'  => [new stdClass(), 'Expected a string. Got: stdClass (stdClass)'],
+    '(true)'            => [true, 'Expected a string. Got: true (bool)'],
+    '(false)'           => [false, 'Expected a string. Got: false (bool)'],
 ]);
