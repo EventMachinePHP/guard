60358754739404caea3e03b7207e2dabd90c5ee5
Yunus Emre Deligöz <ye@deligoz.me>
2023-03-03 03:23:18 +0300

    WIP

---
+++ tests/Guards/IsPrintableCharacterTest.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+use EventMachinePHP\Guard\Guard;
+use EventMachinePHP\Guard\Exceptions\InvalidGuardArgumentException;
+
+/**
+ * This test file contains tests for the {@see Guard::isPrintableCharacter()} method.
+ */
+test(description: passingCasesDescription(__FILE__))
+    ->with(data: passingCasesDataset(__FILE__))
+    ->expect(fn ($value) => (
+        Guard::isPrintableCharacter(
+            value: $value
+        )
+    ))
+    ->toHaveValue(fn ($value) => $value);
+
+test(description: failingCasesDescription(__FILE__))
+    ->with(data: failingCasesDataset(__FILE__))
+    ->expectException(InvalidGuardArgumentException::class)
+    ->expectExceptionMessage(fn ($value, $message) => $message)
+    ->expect(fn ($value, $message) => (
+        Guard::isPrintableCharacter(
+            value: $value
+        )
+    ));
+
+test(description: errorMessagesDescription(__FILE__))
+    ->with(data: randomFailingCase(__FILE__))
+    ->expectExceptionObject(new InvalidGuardArgumentException(message: CUSTOM_ERROR_MESSAGE))
+    ->expectException(InvalidGuardArgumentException::class)
+    ->expect(fn ($value, $message) => (
+        Guard::isPrintableCharacter(
+            value: $value,
+            message: CUSTOM_ERROR_MESSAGE
+        )
+    ));
