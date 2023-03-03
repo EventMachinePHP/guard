5c641f6f00774de35aa657f5571efbd16bf4687f
Yunus Emre Deligöz <ye@deligoz.me>
2023-03-03 03:19:00 +0300

    WIP

--- tests/Datasets/IsGraphicCharacterDataset.php
+++ tests/Datasets/IsGraphicCharacterDataset.php
@@ -3,9 +3,17 @@
 declare(strict_types=1);
 
 dataset(passingCasesDataset(filePath: __FILE__), [
-    [],
+    "('Abc123!')"                                                                                           => ['Abc123!'],
+    "('!\"#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~')" => ['!"#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~'],
 ]);
 
 dataset(failingCasesDataset(filePath: __FILE__), [
-    [],
+    "('Hello, world!')" => ['Hello, world!', 'Expected printable characters except space. Got: "Hello, world!" (string)'],
+    "(' ')"             => [' ', 'Expected printable characters except space. Got: " " (string)'],
+    "('')"              => ['', 'Expected printable characters except space. Got: "" (string)'],
+    '(null)'            => [null, 'Expected a string. Got: null (null)'],
+    '([1, 2, 3])'       => [[1, 2, 3], 'Expected a string. Got: array (array)'],
+    '(new stdClass())'  => [new stdClass(), 'Expected a string. Got: stdClass (stdClass)'],
+    '(true)'            => [true, 'Expected a string. Got: true (bool)'],
+    '(false)'           => [false, 'Expected a string. Got: false (bool)'],
 ]);
---
+++ tests/Guards/IsGraphicCharacterTest.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+use EventMachinePHP\Guard\Guard;
+use EventMachinePHP\Guard\Exceptions\InvalidGuardArgumentException;
+
+/**
+ * This test file contains tests for the {@see Guard::isGraphicCharacter()} method.
+ */
+test(description: passingCasesDescription(__FILE__))
+    ->with(data: passingCasesDataset(__FILE__))
+    ->expect(fn ($value) => (
+        Guard::isGraphicCharacter(
+            value: $value
+        )
+    ))
+    ->toHaveValue(fn ($value) => $value);
+
+test(description: failingCasesDescription(__FILE__))
+    ->with(data: failingCasesDataset(__FILE__))
+    ->expectException(InvalidGuardArgumentException::class)
+    ->expectExceptionMessage(fn ($value, $message) => $message)
+    ->expect(fn ($value, $message) => (
+        Guard::isGraphicCharacter(
+            value: $value
+        )
+    ));
+
+test(description: errorMessagesDescription(__FILE__))
+    ->with(data: randomFailingCase(__FILE__))
+    ->expectExceptionObject(new InvalidGuardArgumentException(message: CUSTOM_ERROR_MESSAGE))
+    ->expectException(InvalidGuardArgumentException::class)
+    ->expect(fn ($value, $message) => (
+        Guard::isGraphicCharacter(
+            value: $value,
+            message: CUSTOM_ERROR_MESSAGE
+        )
+    ));
