a0f3ede7f2a9ab3b12c1b31d6406fc76c73cd51c
Yunus Emre Deligöz <ye@deligoz.me>
2023-03-03 10:04:57 +0300

    feat: Add tests and dataset for hexadecimal digits
    This commit adds tests and datasets for the isHexadecimalDigit guard method, covering valid and invalid input values.
    
    The passingCasesDataset includes valid input values, such as strings containing only hexadecimal digits, while the failingCasesDataset includes invalid input values, such as strings containing non-hexadecimal characters or empty values.
    
    The IsHexadecimalDigitTest file has been updated to use the isHexadecimalDigit method instead of the isAlphanumeric method.

--- tests/Datasets/IsHexadecimalDigitDataset.php
+++ tests/Datasets/IsHexadecimalDigitDataset.php
@@ -3,9 +3,18 @@
 declare(strict_types=1);
 
 dataset(passingCasesDataset(filePath: __FILE__), [
-    [],
+    "('0123456789abcdefABCDEF')"   => ['0123456789abcdefABCDEF'],
+    "('a1b2c3d4e5f6A1B2C3D4E5F6')" => ['a1b2c3d4e5f6A1B2C3D4E5F6'],
+    "('FFF')"                      => ['FFF'],
+    "('0123456789')"               => ['0123456789'],
 ]);
 
 dataset(failingCasesDataset(filePath: __FILE__), [
-    [],
+    "('abcdefg')"      => ['abcdefg', 'Expected hexadecimal digits only. Got: "abcdefg" (string)'],
+    "('')"             => ['', 'Expected hexadecimal digits only. Got: "" (string)'],
+    '(null)'           => [null, 'Expected a string. Got: null (null)'],
+    '([1, 2, 3])'      => [[1, 2, 3], 'Expected a string. Got: array (array)'],
+    '(new stdClass())' => [new stdClass(), 'Expected a string. Got: stdClass (stdClass)'],
+    '(true)'           => [true, 'Expected a string. Got: true (bool)'],
+    '(false)'          => [false, 'Expected a string. Got: false (bool)'],
 ]);
