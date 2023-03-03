5321a6661ca73aef8fed73114a114a06246a5d9b
Yunus Emre Deligöz <ye@deligoz.me>
2023-03-03 03:09:10 +0300

    WIP

--- tests/Datasets/IsLowercaseDataset.php
+++ tests/Datasets/IsLowercaseDataset.php
@@ -3,9 +3,18 @@
 declare(strict_types=1);
 
 dataset(passingCasesDataset(filePath: __FILE__), [
-    [],
+    "('abc')"                        => ['abc'],
+    "('hello')"                      => ['hello'],
+    "('abcdefghijklmnopqrstuvwxyz')" => ['abcdefghijklmnopqrstuvwxyz'],
 ]);
 
 dataset(failingCasesDataset(filePath: __FILE__), [
-    [],
+    "('Abc123')"       => ['Abc123', 'Expected lowercase characters only. Got: "Abc123" (string)'],
+    "('ABC')"          => ['ABC', 'Expected lowercase characters only. Got: "ABC" (string)'],
+    "('')"             => ['', 'Expected lowercase characters only. Got: "" (string)'],
+    '(null)'           => [null, 'Expected a string. Got: null (null)'],
+    '([1, 2, 3])'      => [[1, 2, 3], 'Expected a string. Got: array (array)'],
+    '(new stdClass())' => [new stdClass(), 'Expected a string. Got: stdClass (stdClass)'],
+    '(true)'           => [true, 'Expected a string. Got: true (bool)'],
+    '(false)'          => [false, 'Expected a string. Got: false (bool)'],
 ]);
---
+++ tests/Guards/IsLowercaseTest.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+use EventMachinePHP\Guard\Guard;
+use EventMachinePHP\Guard\Exceptions\InvalidGuardArgumentException;
+
+/**
+ * This test file contains tests for the {@see Guard::isLowercase()} method.
+ */
+test(description: passingCasesDescription(__FILE__))
+    ->with(data: passingCasesDataset(__FILE__))
+    ->expect(fn ($value) => (
+        Guard::isLowercase(
+            value: $value
+        )
+    ))
+    ->toHaveValue(fn ($value) => $value);
+
+test(description: failingCasesDescription(__FILE__))
+    ->with(data: failingCasesDataset(__FILE__))
+    ->expectException(InvalidGuardArgumentException::class)
+    ->expectExceptionMessage(fn ($value, $message) => $message)
+    ->expect(fn ($value, $message) => (
+        Guard::isLowercase(
+            value: $value
+        )
+    ));
+
+test(description: errorMessagesDescription(__FILE__))
+    ->with(data: randomFailingCase(__FILE__))
+    ->expectExceptionObject(new InvalidGuardArgumentException(message: CUSTOM_ERROR_MESSAGE))
+    ->expectException(InvalidGuardArgumentException::class)
+    ->expect(fn ($value, $message) => (
+        Guard::isLowercase(
+            value: $value,
+            message: CUSTOM_ERROR_MESSAGE
+        )
+    ));
