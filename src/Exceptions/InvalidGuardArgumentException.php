8ef1e6c0ddd9adce4d99b0d74509469baf496019
Yunus Emre Deligöz <ye@deligoz.me>
2024-02-05 07:32:58 +0300

    Refactor InvalidGuardArgumentException class
    The InvalidGuardArgumentException class has been refactored to handle ExceptionMessage objects in addition to string messages. This enables more flexible error messaging by allowing either string or object input and providing clearer constructor logic.

--- src/Exceptions/InvalidGuardArgumentException.php
+++ src/Exceptions/InvalidGuardArgumentException.php
@@ -4,13 +4,23 @@ declare(strict_types=1);
 
 namespace EventMachinePHP\Guard\Exceptions;
 
-class InvalidGuardArgumentException extends \InvalidArgumentException
+use InvalidArgumentException;
+use EventMachinePHP\Guard\ExceptionMessage;
+
+class InvalidGuardArgumentException extends InvalidArgumentException
 {
     public static function create(
+        string|ExceptionMessage $defaultMessage,
         ?string $customMessage = null,
-        ?string $defaultMessage = null,
         ?array $values = null,
     ): self {
-        return new self($customMessage ?: sprintf($defaultMessage, ...$values));
+        if ($customMessage !== null) {
+            return new self($customMessage);
+        }
+
+        $defaultMessage = is_string($defaultMessage) ? $defaultMessage : $defaultMessage->value;
+        $message        = $defaultMessage.' '.ExceptionMessage::ValueMessage->value;
+
+        return new self(sprintf($message, ...$values));
     }
 }
