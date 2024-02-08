7a6a586ecce01a7ef0924e945ba3e1e297ac3808
Yunus Emre Deligöz <ye@deligoz.me>
2024-02-08 15:37:37 +0300

    Add tests for isArray guard method
    A new test file, IsArrayTest.php, has been added to ArrayGuards. These tests are for the Guard::isArray() method, which checks if the given value is an array, handling both passing and failing cases. It also includes a specific test for custom error messages.

---
+++ tests/Guards/ArrayGuards/IsArrayTest.php
@@ -0,0 +1,64 @@
+<?php
+
+/**
+ * This test file contains tests for the {@see Guard::isArray()} method.
+ */
+
+use EventMachinePHP\Guard\ExceptionMessage;
+use EventMachinePHP\Guard\Exceptions\InvalidGuardArgumentException;
+use EventMachinePHP\Guard\Guard;
+use EventMachinePHP\Guard\Tests\GuardTestCase;
+
+const IS_ARRAY_PASSING_CASES = [
+    GuardTestCase::A001_ARRAY_EMPTY,
+    GuardTestCase::A002_ARRAY_INTEGER_INDEXED,
+    GuardTestCase::A003_ARRAY_INGEGER_NEGATIVE_INDEXED,
+    GuardTestCase::A004_ARRAY_FLOAT_INDEXED,
+    GuardTestCase::A005_ARRAY_FLOAT_NEGATIVE_INDEXED,
+    GuardTestCase::A006_ARRAY_BOOLEAN_TRUE_INDEXED,
+    GuardTestCase::A007_ARRAY_BOOLEAN_FALSE_INDEXED,
+    GuardTestCase::A008_ARRAY_ASSOCIATIVE_NULL_WITH_EMPTY_KEY,
+    GuardTestCase::A009_ARRAY_ASSOCIATIVE_EMPTY_WITH_EMPTY_KEY,
+    GuardTestCase::A010_ARRAY_ASSOCIATIVE_EMPTY,
+    GuardTestCase::A011_ARRAY_NULL_VALUE,
+    GuardTestCase::A012_ARRAY_ASSOCIATIVE_NULL_VALUE,
+    GuardTestCase::A013_ARRAY_FALSE_VALUE,
+    GuardTestCase::A014_ARRAY_ASSOCIATIVE_FALSE_VALUE,
+    GuardTestCase::A015_ARRAY_TRUE_AND_FALSE,
+    GuardTestCase::A016_ARRAY_ASSOCIATIVE_TRUE_AND_FALSE,
+    GuardTestCase::A017_ARRAY_NULL_TRUE_FALSE,
+    GuardTestCase::A018_ARRAY_ASSOCIATIVE_NULL_TRUE_FALSE,
+    GuardTestCase::A019_ARRAY_ZERO,
+    GuardTestCase::A020_ARRAY_ASSOCIATIVE_ZERO,
+    GuardTestCase::A021_ARRAY_NEGATIVE_ZERO,
+    GuardTestCase::A022_ARRAY_FLOAT_ZER0,
+    GuardTestCase::A023_ARRAY_FLOAT_ZER0_NEGATIVE,
+    GuardTestCase::A024_ARRAY_POSITIVE_INTEGERS,
+    GuardTestCase::A025_ARRAY_NEGATIVE_INTEGERS,
+    GuardTestCase::A026_ARRAY_NEGATIVE_FLOATS,
+    GuardTestCase::A027_ARRAY_OBJECTS,
+];
+
+test('isArray(passing)', function (mixed $value): void {
+    $result = Guard::isArray(value: $value);
+
+    expect($result)
+        ->toBeArray()
+        ->toBe($value);
+})->with(testCases(IS_ARRAY_PASSING_CASES));
+
+test('isArray(failing)', function (mixed $value): void {
+    expect(fn() => Guard::isArray(value: $value))
+        ->toThrow(
+            exception: InvalidGuardArgumentException::class,
+            exceptionMessage: ExceptionMessage::IsArray->value
+        );
+})->with(allCases(except: IS_ARRAY_PASSING_CASES));
+
+test('isArray(message)', function (mixed $value): void {
+    expect(fn() => Guard::isArray(value: $value, message: CUSTOM_ERROR_MESSAGE))
+        ->toThrow(
+            exception: InvalidGuardArgumentException::class,
+            exceptionMessage: CUSTOM_ERROR_MESSAGE
+        );
+})->with(randomCase(except: IS_ARRAY_PASSING_CASES));
